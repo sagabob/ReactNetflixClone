d0aebbe68e46fd2465f83ca2a070a456eab916fb
DESK-TDP-01\bobpham <bobpham.tdp@gmail.com>
2023-01-13 20:07:51 +1100

    add global styles

---
+++ src/global_styles.tsx
@@ -0,0 +1,23 @@
+import { Global, css } from "@emotion/react";
+
+const GlobalStyles = css`
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+  html,
+  body {
+    height: 100%;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: #000000;
+    color: #333333;
+    font-size: 16px;
+  }
+`;
+
+export default GlobalStyles;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Global } from "@emotion/react";
+import GlobalStyles from "./global_styles";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <Global styles={GlobalStyles} />
     <App />
   </React.StrictMode>
 );
