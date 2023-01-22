67446581336cefc5909b0dd1cad9c4fb3f3c8f1b
bobp-id <75350226+bobp-id@users.noreply.github.com>
2023-01-23 02:57:47 +1100

    work in progress

--- src/components/accordion/types.tsx
+++ src/components/accordion/types.tsx
@@ -1,17 +1,17 @@
 
-export interface AccordionProps extends React.ComponentPropsWithoutRef<"div"> {}
+export interface AccordionProps extends React.ComponentPropsWithoutRef<"div"> { }
 
-export interface AccordionFrameProps extends React.ComponentPropsWithoutRef<"div"> {}
+export interface AccordionFrameProps extends React.ComponentPropsWithoutRef<"div"> { }
 
-export interface AccordionItemProps extends React.ComponentPropsWithoutRef<"div"> {}
+export interface AccordionItemProps extends React.ComponentPropsWithoutRef<"div"> { }
 
 export interface AccordionHeaderProps extends React.ComponentPropsWithoutRef<"div"> {
-    itemName:string;
-   
+    itemName: string;
+
 }
 
 export interface AccordionBodyProps extends React.ComponentPropsWithoutRef<"div"> {
-    itemName:string;   
+    itemName: string;
 }
 
-export interface AccordionTitleProps extends React.ComponentPropsWithoutRef<"h1"> {}
+export interface AccordionTitleProps extends React.ComponentPropsWithoutRef<"h1"> { }
