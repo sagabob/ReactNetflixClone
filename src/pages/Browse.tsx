4d652100a124fcee32c0d4e7e7680210b2c957fd
bobp-id <75350226+bobp-id@users.noreply.github.com>
2023-02-09 20:35:26 +1100

    work in progress

--- src/pages/Browse.tsx
+++ src/pages/Browse.tsx
@@ -1,6 +1,5 @@
 import { BannerContainer } from "../containers/banner";
 import { NavbarContainer } from "../containers/navbar";
-import { SilderBannerContainer } from "../containers/slider_banner";
 import requests from "../data/request";
 
 
@@ -8,8 +7,7 @@ export default function Browse() {
     return (
         <>
             <NavbarContainer />
-            <BannerContainer fetchUrl={requests.fetchNetflixOriginals} />
-            
+            <BannerContainer fetchUrl={requests.fetchNetflixOriginals} />            
         </>
     )
 }
