93ee8d778cd256ab7a76fcd36b8bc08d62c8797e
bobp-id <75350226+bobp-id@users.noreply.github.com>
2023-02-09 21:41:50 +1100

    work in progress

--- src/containers/header_section.tsx
+++ src/containers/header_section.tsx
@@ -7,8 +7,8 @@ const HeaderSection = (props: HeaderProps) => {
     <Header src="home-bg">
       <Header.Frame>
         <Header.Logo to={ROUTES.HOME} src="/images/logo.svg" alt="Netflix" />
-        <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign In</Header.ButtonLink>
         <Header.ButtonLink to={ROUTES.BROWSE}>Browse</Header.ButtonLink>
+          
       </Header.Frame>
       {props.children}
     </Header>
